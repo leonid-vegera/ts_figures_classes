45023c302ddbe986e0e007b26521d665bc030618
Leonid Vegera <leonid.vegera@gmail.com>
2021-12-13 20:57:27 +0200

    Done

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,19 +1,79 @@
+type Color = 'red' | 'green' | 'blue';
+type Shape = 'triangle' | 'circle' | 'rectangle';
 export interface Figure {
-
+  shape: Shape;
+  color: Color;
+  getArea(): number;
 }
 
-export class Triangle {
+export class Triangle implements Figure {
+  public shape: Shape = 'triangle';
 
+  constructor(
+    public color: Color,
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('Sides should be not less than 0');
+    }
+
+    if ((a + b <= c)
+      || (c + b <= a)
+      || (a + c <= b)) {
+      throw new Error('Not valid sides!');
+    }
+  }
+
+  getArea(): number {
+    const { a, b, c } = this;
+    const p = (a + b + c) / 2;
+    const result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    return +result.toFixed(2);
+  }
 }
 
-export class Circle {
+export class Circle implements Figure {
+  public shape: Shape = 'circle';
 
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Sides should be not less than 0!');
+    }
+  }
+
+  getArea(): number {
+    const { radius } = this;
+
+    return Math.floor((Math.PI * radius ** 2) * 100) / 100;
+  }
 }
 
-export class Rectangle {
+export class Rectangle implements Figure {
+  public shape: Shape = 'rectangle';
 
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Sides should be not less than 0!');
+    }
+  }
+
+  getArea(): number {
+    const { width, height } = this;
+
+    return +(width * height).toFixed(2);
+  }
 }
 
-export function getInfo(figure) {
-
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
